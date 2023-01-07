c651d1e9e3728f4cf6ac7603a5207532c6259eeb
rickytsao <ricky.tsao@hkjc.org.hk>
2023-01-07 13:18:40 +0800

    questionnaire-node: enable cross origin requests

--- questionnaire-node/server.js
+++ questionnaire-node/server.js
@@ -20,6 +20,12 @@ config(); // configure to read env file
 
 const port = process.env.PORT || 8080;
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "rickyabc.com"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 // routes
 app.get('/', (req, res) => {
     try {
